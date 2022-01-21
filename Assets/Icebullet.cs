2e115e01f383eb3b39c029339df7ce296c9e4683
AWPIram <iram.ahmed@elev.ga.lbs.se>
2022-01-21 13:35:02 +0100

    basic

---
+++ Assets/Icebullet.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icebullet : MonoBehaviour
+{
+    float timer;
+    bool lazer;
+    public GameObject iceblock;
+
+    public static float ispeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ispeed = 5;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      
+
+        transform.position += new Vector3(ispeed, 0, 0) * Time.deltaTime;
+
+
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(iceblock, transform.position, iceblock.transform.rotation);
+
+
+        if (true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
